75abe4f5cbb2f6ccd8ae57d4d82c8d6800ae3672
jonas <125613982+jonasalameda@users.noreply.github.com>
2024-04-22 15:16:14 -0400

    add another println() in main

--- src/main/java/org/jonas/Main.java
+++ src/main/java/org/jonas/Main.java
@@ -3,5 +3,6 @@ package org.jonas;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is a new project");
+        System.out.println("Using git bash for VC");
     }
 }
